a32b8e5b4b49f1a35789d0c4c87b892b572ce3a9
gameislife <88309995+gameislife@users.noreply.github.com>
2023-03-18 18:41:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,89 @@
-$(document).ready(function () {
-  var noOfProduct = $("#product_container ul li").length;
-  var totalProductWidth = 0;
-  for (var i = 0; i < noOfProduct; i++) {
-        var productWidth = $("#product_container ul li").eq(i).outerWidth(true);
-        totalProductWidth = totalProductWidth + productWidth;
-  }
+// <!--------------------------------------------------- SHOW MENU ---------------------------------------------------------->
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-  $("#slider").css("width", totalProductWidth + "px");
+/* Validate if constant exists - show menu */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
-  var speed = 1;
-  animateProducts();
-
-  function animateProducts() {
-        $("#slider li")
-              .eq(0)
-              .animate(
-                    {
-                          marginLeft: "-=" + speed + "px"
-                    },
-                    1,
-                    function () {
-                          var animProductWidth = $(this).outerWidth(true);
-
-                          if (speed >= animProductWidth) {
-                                $(this).parent().append($(this));
-                                $(this).removeAttr("style");
-                          }
-
-                          aninInterval = setTimeout(function () {
-                                animateProducts();
-                          });
-                    }
-              );
-  }
-
-  $("#slider").hover(
-        function () {
-              clearTimeout(aninInterval);
-              $("#slider li").eq(0).stop();
-        },
-        function () {
-              animateProducts();
-        }
-  );
+/* Validate if constant exists - hide menu */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 
-});
+// <!-------------------------------------------------- MENU REMOVE ---------------------------------------------------------->
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+// <!-------------------------------------------------- CHANGING BG HEADER---------------------------------------------------------->
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+
+// <!--------------------------------------------------- GAMES SWIPER ---------------------------------------------------------->
+var swiper = new Swiper(".games_container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 30,
+    coverflowEffect:{
+        rotate: 0,
+    },
+})
+
+
+
+
+
+// <!-------------------------------------------------- ANIMATION - SECTIONS ---------------------------------------------------------->
+
+
+// const sr = ScrollReveal({
+//     distance: '60px',
+//     duration: 2800,
+//     // reset: true,
+//     })
+
+
+// sr.reveal(`.home_data, .home_social_icons, .trending,
+//            .games_container,
+//            .specs_data, .specs_overlay,
+//            .packs_card,
+//            .footer_data, .footer_rights, .section__title,
+//            .video_description, .specs_container,
+//            .comments_heading, .comments_container, .iframe-container, .service`,{
+//     origin: 'top',
+//     interval: 60,
+// })
+
+// sr.reveal(`.aboutus_data`,{
+//     origin: 'left',
+// })
+
+// sr.reveal(`.aboutus_img`,{
+//     origin: 'right',
+//     interval: 60,
+// })
